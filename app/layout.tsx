a32b8214c1f08ad68f408c421dd72187ccdd2655
wonza-hub <wonza8585@gmail.com>
2025-07-06 22:52:29 +0900

    design: 홈 레이아웃 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <ApolloWrapper>
         <body>
-          <div className="relative mx-auto min-h-[calc(var(--vh,1vh)*100)] w-full bg-white px-2 py-0 shadow-[0px_0px_20px_0px_#191E2333]">
-            <header className="h-top-navbar sticky top-0 left-0 z-[10000000] flex w-full flex-row items-center justify-between px-[16px] shadow-[inset_0px_-1px_0px_0px_rgba(255,255,255,0.15)]">
+          <div className="relative mx-auto min-h-[calc(var(--vh,1vh)*100)] w-full bg-white py-0">
+            <header className="h-top-navbar sticky top-0 left-0 z-[10000000] flex w-full flex-row items-center justify-between bg-white px-[16px]">
               <Link href={'/'}>CluBNU</Link>
               <Link href={'/signin'}>로그인</Link>
             </header>
